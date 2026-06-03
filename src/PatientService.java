7cecfc89c078b69f07a31e4b4e147f5fd06d3316
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-03 11:24:18 +0530

    Service module created and addPatient method added...

---
+++ src/PatientService.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class PatientService {
+    ArrayList<Patient> patients = new ArrayList<>();
+
+    public void addPatient(Patient patient){
+        patients.add(patient);
+        System.out.println("Patient Added SUccessfully...");
+    }
+}
